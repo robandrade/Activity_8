902782b0c7be4e666a12ccde2853b39c13312c48
robandrade <103383943+robandrade@users.noreply.github.com>
2022-04-17 23:43:11 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Activity_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fatLabel = new System.Windows.Forms.Label();
+            this.fatConsumed = new System.Windows.Forms.TextBox();
+            this.fatButton = new System.Windows.Forms.Button();
+            this.fatCalories = new System.Windows.Forms.Label();
+            this.carbsLabel = new System.Windows.Forms.Label();
+            this.carbsConsumed = new System.Windows.Forms.TextBox();
+            this.carbsButton = new System.Windows.Forms.Button();
+            this.carbsCalories = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fatLabel
+            // 
+            this.fatLabel.AutoSize = true;
+            this.fatLabel.Location = new System.Drawing.Point(34, 31);
+            this.fatLabel.Name = "fatLabel";
+            this.fatLabel.Size = new System.Drawing.Size(148, 13);
+            this.fatLabel.TabIndex = 0;
+            this.fatLabel.Text = "Enter  grams of fat consumed:";
+            // 
+            // fatConsumed
+            // 
+            this.fatConsumed.Location = new System.Drawing.Point(218, 28);
+            this.fatConsumed.Name = "fatConsumed";
+            this.fatConsumed.Size = new System.Drawing.Size(100, 20);
+            this.fatConsumed.TabIndex = 1;
+            // 
+            // fatButton
+            // 
+            this.fatButton.Location = new System.Drawing.Point(334, 20);
+            this.fatButton.Name = "fatButton";
+            this.fatButton.Size = new System.Drawing.Size(75, 35);
+            this.fatButton.TabIndex = 2;
+            this.fatButton.Text = "Calculate";
+            this.fatButton.UseVisualStyleBackColor = true;
+            this.fatButton.Click += new System.EventHandler(this.fatButton_Click_1);
+            // 
+            // fatCalories
+            // 
+            this.fatCalories.AutoSize = true;
+            this.fatCalories.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fatCalories.Location = new System.Drawing.Point(34, 64);
+            this.fatCalories.Name = "fatCalories";
+            this.fatCalories.Size = new System.Drawing.Size(0, 13);
+            this.fatCalories.TabIndex = 3;
+            // 
+            // carbsLabel
+            // 
+            this.carbsLabel.AutoSize = true;
+            this.carbsLabel.Location = new System.Drawing.Point(34, 102);
+            this.carbsLabel.Name = "carbsLabel";
+            this.carbsLabel.Size = new System.Drawing.Size(166, 13);
+            this.carbsLabel.TabIndex = 4;
+            this.carbsLabel.Text = "Enter the grams of carbohydrates:";
+            // 
+            // carbsConsumed
+            // 
+            this.carbsConsumed.Location = new System.Drawing.Point(218, 102);
+            this.carbsConsumed.Name = "carbsConsumed";
+            this.carbsConsumed.Size = new System.Drawing.Size(100, 20);
+            this.carbsConsumed.TabIndex = 5;
+            // 
+            // carbsButton
+            // 
+            this.carbsButton.Location = new System.Drawing.Point(334, 93);
+            this.carbsButton.Name = "carbsButton";
+            this.carbsButton.Size = new System.Drawing.Size(75, 36);
+            this.carbsButton.TabIndex = 6;
+            this.carbsButton.Text = "Calculate";
+            this.carbsButton.UseVisualStyleBackColor = true;
+            this.carbsButton.Click += new System.EventHandler(this.carbsButton_Click);
+            // 
+            // carbsCalories
+            // 
+            this.carbsCalories.AutoSize = true;
+            this.carbsCalories.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carbsCalories.Location = new System.Drawing.Point(34, 136);
+            this.carbsCalories.Name = "carbsCalories";
+            this.carbsCalories.Size = new System.Drawing.Size(0, 13);
+            this.carbsCalories.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 205);
+            this.Controls.Add(this.carbsCalories);
+            this.Controls.Add(this.carbsButton);
+            this.Controls.Add(this.carbsConsumed);
+            this.Controls.Add(this.carbsLabel);
+            this.Controls.Add(this.fatCalories);
+            this.Controls.Add(this.fatButton);
+            this.Controls.Add(this.fatConsumed);
+            this.Controls.Add(this.fatLabel);
+            this.Name = "Form1";
+            this.Text = "Calculate Calories";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fatLabel;
+        private System.Windows.Forms.TextBox fatConsumed;
+        private System.Windows.Forms.Button fatButton;
+        private System.Windows.Forms.Label fatCalories;
+        private System.Windows.Forms.Label carbsLabel;
+        private System.Windows.Forms.TextBox carbsConsumed;
+        private System.Windows.Forms.Button carbsButton;
+        private System.Windows.Forms.Label carbsCalories;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Create method for calculating the grams of fat into calories method
+        private int FatCalories(int fat) 
+        {
+            return fat * 9;
+        }
+
+        // main method button that will then call the FatCalories method to recieve the data to print out calculation
+        private void fatButton_Click_1(object sender, EventArgs e)
+        {
+            int fatGrams;
+            int caloriesFat;
+
+            if (int.TryParse(fatConsumed.Text, out fatGrams))
+            {
+                caloriesFat = FatCalories(fatGrams);
+                fatCalories.Text = "Calories from grams of fat consumed: " + caloriesFat + "g";
+            }
+        }
+
+        // Create method for calculating the grams of carbs into calories method 
+        private int CarbCalories(int carbs)
+        {
+            return carbs * 4;
+        }
+
+        // main method button that will then call the CarbCalories method to recieve the data to print out calculation
+        private void carbsButton_Click(object sender, EventArgs e)
+        {
+            int carbsGrams;
+            int caloriesCarbs;
+
+            if (int.TryParse(carbsConsumed.Text, out carbsGrams))
+            {
+                caloriesCarbs = CarbCalories(carbsGrams);
+                carbsCalories.Text = "Calories from grams of carbohydrates consumed: " + caloriesCarbs + "g";
+            }
+        }
+    }
+}
